b5db2a3a4c709eb01e24a5f961b5157a71d2b4c4
alexSfFeng <alex.sf.feng@gmail.com>
2017-07-19 17:48:02 -0700

    Fixed month and year panel, need to implement the days of the calendar

--- src/calendarFunctions/MainCalendar.java
+++ src/calendarFunctions/MainCalendar.java
@@ -8,7 +8,6 @@ import java.util.Calendar;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -24,22 +23,24 @@ import javax.swing.JPanel;
 public class MainCalendar extends JPanel {
 
   // top main container panel
-  private JPanel topContainel, topYearPanel, topMonthPanel;
+  private JPanel topContainel, topYearPanel, topMonthPanel, monthSubPanel,
+      yearSubPanel;
   private JButton nextYearButton, prevYearButton, nextMonButton, prevMonButton,
       yearButton, monthButton;
 
   // appearance constants
   private static final int PANEL_WIDTH = 200;
   private static final int PANEL_HEIGHT = 120;
+  private static final int NXT_PRV_FONT = 29;
+  private static final int MON_FONT = 31;
+  private static final int YEAR_FONT = 35;
 
   // padding in the top panel for better appearance
-  private static final int BORDER_PADDING = 13;
   private static final int BUTTON_PADDING = 175;
   private static final int BORDER_STROKE = 2;
-
   private static final int MON_WIDTH = 280;
   private static final int MON_HEIGHT = 41;
-  private static final int YEAR_WIDTH = 280;
+  private static final int YEAR_WIDTH = 180;
   private static final int YEAR_HEIGHT = 55;
 
   // Calendar constant strings
@@ -78,52 +79,59 @@ public class MainCalendar extends JPanel {
     topContainel.setBackground(CALENDAR_COLOR);
 
     /*--------- setting up the year panel---------------------*/
-    topYearPanel = new JPanel();
-    topYearPanel.setLayout(new BoxLayout(topYearPanel, BoxLayout.X_AXIS));
+    topYearPanel = new JPanel(new BorderLayout());
     topYearPanel
         .setBorder(BorderFactory.createLineBorder(Color.BLACK, BORDER_STROKE));
 
     // making the year buttons
     nextYearButton = new JButton(">>");
-    nextYearButton.setFont(new Font("Marker Felt", Font.BOLD, 29));
+    nextYearButton.setFont(new Font("Marker Felt", Font.BOLD, NXT_PRV_FONT));
     prevYearButton = new JButton("<<");
-    prevYearButton.setFont(new Font("Marker Felt", Font.BOLD, 29));
+    prevYearButton.setFont(new Font("Marker Felt", Font.BOLD, NXT_PRV_FONT));
     yearButton = new JButton("Year: " + currentCal.get(Calendar.YEAR));
-    yearButton.setFont(new Font("Marker Felt", Font.BOLD, 35));
+    yearButton.setFont(new Font("Marker Felt", Font.BOLD, YEAR_FONT));
     yearButton.setPreferredSize(new Dimension(YEAR_WIDTH, YEAR_HEIGHT));
 
+    // create rigid area
+    yearSubPanel = new JPanel(new BorderLayout());
+    yearSubPanel.add(Box.createRigidArea(new Dimension(BUTTON_PADDING,0)),BorderLayout.WEST);
+    yearSubPanel.add(Box.createRigidArea(new Dimension(BUTTON_PADDING,0)),BorderLayout.EAST);
+    yearSubPanel.add(yearButton);
+    
     // adding the button to the year panel
-    topYearPanel.add(Box.createRigidArea(new Dimension(BORDER_PADDING, 0)));
-    topYearPanel.add(prevYearButton);
-    topYearPanel.add(Box.createRigidArea(new Dimension(BUTTON_PADDING, 0)));
-    topYearPanel.add(yearButton);
-    topYearPanel.add(Box.createRigidArea(new Dimension(BUTTON_PADDING, 0)));
-    topYearPanel.add(nextYearButton);
-    topYearPanel.add(Box.createRigidArea(new Dimension(BORDER_PADDING, 0)));
+    topYearPanel.add(prevYearButton, BorderLayout.WEST);
+    topYearPanel.add(nextYearButton, BorderLayout.EAST);
+    topYearPanel.add(yearSubPanel);
 
     /*---------- setting up the month panel ----------------*/
-    topMonthPanel = new JPanel();
-    topMonthPanel.setLayout(new BoxLayout(topMonthPanel, BoxLayout.X_AXIS));
+    topMonthPanel = new JPanel(new BorderLayout());
     topMonthPanel
         .setBorder(BorderFactory.createLineBorder(Color.BLACK, BORDER_STROKE));
 
     // making the month buttons
     nextMonButton = new JButton(">");
-    nextMonButton.setFont(new Font("Marker Felt", Font.BOLD, 29));
+    nextMonButton.setFont(new Font("Marker Felt", Font.BOLD, NXT_PRV_FONT));
     prevMonButton = new JButton("<");
-    prevMonButton.setFont(new Font("Marker Felt", Font.BOLD, 29));
+    prevMonButton.setFont(new Font("Marker Felt", Font.BOLD, NXT_PRV_FONT));
     monthButton = new JButton(MONS_OF_YEAR[currentCal.get(Calendar.MONTH)]);
-    monthButton.setFont(new Font("Marker Felt", Font.BOLD, 31));
+    monthButton.setFont(new Font("Marker Felt", Font.BOLD, MON_FONT));
     monthButton.setPreferredSize(new Dimension(MON_WIDTH, MON_HEIGHT));
+    today = new JButton("Today");
+    today.setFont(new Font("Marker Felt", Font.ITALIC, 29));
+    today.setPreferredSize(new Dimension(BUTTON_PADDING, MON_HEIGHT));
+
+    // create rigid area
+    monthSubPanel = new JPanel(new BorderLayout());
+    monthSubPanel.add(Box.createRigidArea(new Dimension(BUTTON_PADDING, 0)),
+        BorderLayout.WEST);
+    monthSubPanel.add(today,
+        BorderLayout.EAST);
+    monthSubPanel.add(monthButton);
 
     // adding the button to the year panel
-    topMonthPanel.add(Box.createRigidArea(new Dimension(BORDER_PADDING, 0)));
-    topMonthPanel.add(prevMonButton);
-    topMonthPanel.add(Box.createRigidArea(new Dimension(BUTTON_PADDING, 0)));
-    topMonthPanel.add(monthButton);
-    topMonthPanel.add(Box.createRigidArea(new Dimension(BUTTON_PADDING, 0)));
-    topMonthPanel.add(nextMonButton);
-    topMonthPanel.add(Box.createRigidArea(new Dimension(BORDER_PADDING, 0)));
+    topMonthPanel.add(prevMonButton, BorderLayout.WEST);
+    topMonthPanel.add(nextMonButton, BorderLayout.EAST);
+    topMonthPanel.add(monthSubPanel);
 
     // adding the year and month panel together to the top
     topContainel.setPreferredSize(new Dimension(400, 90));
